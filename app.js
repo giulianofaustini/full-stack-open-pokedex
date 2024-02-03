935020cf305df3932df4c3a35fdc5a161fb8d68a
Giuliano Faustini <giuliperus@gmail.com>
2024-02-03 18:23:55 +0200

    ok health?

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static("dist"));
 app.get('/version', (req, res) => {
   res.send('This is one with two') // change this string to ensure a new version deployed
 })
-app.get('/health', (req, res) => { 
+app.get('/health', (req, res) => {
   res.send('ok')
 })
 
