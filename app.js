256ae0aab52421fc4d4fb835281cfbdf6fa013c4
Giuliano Faustini <giuliperus@gmail.com>
2024-02-03 17:57:22 +0200

    health

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/version', (req, res) => {
   res.send('This is one with two') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
